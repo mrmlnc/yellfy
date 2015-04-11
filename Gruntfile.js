56b0d439d9620a4fb2b7c04c0c7200f6f0bbc911
Denis Malinochkin <dmalinochkin@land.ru>
2015-04-11 20:03:36 +0300

    Postinstall script

--- Gruntfile.js
+++ Gruntfile.js
@@ -359,6 +359,23 @@ var configureGrunt = function(grunt) {
   });
 
   grunt.registerTask('images', ['imagemin']);
+
+  // Task for NPM postinstall
+  // Create `vendor` directories
+  grunt.registerTask('postinstall', function() {
+    var vendorDirs = [
+      './app/fonts/',
+      './app/images/icons/',
+      './app/scripts/vendor/',
+      './app/styles/vendor/'
+    ];
+    vendorDirs.forEach(function(dir) {
+      if (!grunt.file.exists(dir)) {
+        grunt.file.mkdir(dir);
+      }
+    });
+    grunt.log.ok('The project is now ready for use!');
+  });
 };
 
 // Export the configuration

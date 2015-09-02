dbaace34845d24b54c9377559fd0e866fc8b5210
Denis Malinochkin <dmalinochkin@rambler.ru>
2015-09-02 16:10:19 +0300

    The error handler in the task wiredep

--- config/plugins/wiredep.js
+++ config/plugins/wiredep.js
@@ -1,14 +1,13 @@
-/**
- * ## grunt-wiredep
- * Inject Bower dependencies right into your HTML from Grunt
- *
- */
-
-module.exports = {
-
-  main: {
-    ignorePath: /^\/|\.\.\/bower_components\//,
-    src: ['build/*.html']
-  }
-
+module.exports = function(grunt) {
+  return {
+    main: {
+      ignorePath: /^\/|\.\.\/bower_components\//,
+      src: ['build/*.html'],
+      onError: function(err) {
+        if (err.code === 'BOWER_COMPONENTS_MISSING') {
+          grunt.log.error('Warning: Cannot find where you keep your Bower packages.');
+        }
+      }
+    }
+  };
 };

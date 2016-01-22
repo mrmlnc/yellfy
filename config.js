0388cdc7db25cc3e557dbd6b629bec0e8ac251f4
Denis Malinochkin <dmalinochkin@rambler.ru>
2016-01-22 16:05:13 +0300

    Correction of the supported browser versions

--- config.js
+++ config.js
@@ -24,14 +24,14 @@ module.exports = {
   },
   autoprefixer: [
     // Microsoft
-    'Explorer >= 9',
+    'Explorer >= 10',
     'Edge >= 12',
     'ExplorerMobile >= 10',
     // Mozilla
     'Firefox >= 30',
     // Google
     'Chrome >= 34',
-    'Android >= 4',
+    'Android >= 4.4',
     // Opera
     'Opera >= 12',
     // Apple

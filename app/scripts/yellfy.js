5d9bd73734c8757490324d273947ef2bcf9fbbc9
Denis Malinochkin <dmalinochkin@rambler.ru>
2016-03-19 01:15:00 +0300

    Use fetch API for example

--- app/scripts/yellfy.js
+++ app/scripts/yellfy.js
@@ -1,27 +1,12 @@
+'use strict';
+
 (() => {
-  const req = new XMLHttpRequest();
-  const _responseHandler = (responseText) => {
-    let latestTag;
-    try {
-      latestTag = JSON.parse(responseText)[0];
-    } catch (err) {
-      throw new Error('Ha-ha, GitHub! Bad JSON.');
-    }
-
-    console.log(`The latest version of Yellfy — ${latestTag.name}!`);
-  };
-
-  req.open('GET', 'https://api.github.com/repos/mrmlnc/yellfy/tags', true);
-  req.onreadystatechange = () => {
-    if (req.readyState === 4) {
-      if (req.status === 200) {
-        _responseHandler(req.responseText);
-        return;
-      }
-
-      throw new Error('Error loading page\n');
-    }
-  };
-
-  req.send(null);
+  fetch('https://api.github.com/repos/mrmlnc/yellfy/tags', { method: 'GET' })
+    .then((res) => res.json())
+    .then((tags) => {
+      console.log(`The latest version of Yellfy — ${tags[0].name}!`);
+    })
+    .catch((err) => {
+      throw new Error(err);
+    });
 })();

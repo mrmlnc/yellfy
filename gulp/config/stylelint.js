ef05fce8012b7ce4a5961afd6c4cb4dea85b815f
Denis Malinochkin <dmalinochkin@rambler.ru>
2016-08-01 18:45:29 +0300

    Update stylelint config

--- gulp/config/stylelint.js
+++ gulp/config/stylelint.js
@@ -4,6 +4,7 @@ module.exports = {
   extends: 'stylelint-config-standard',
   rules: {
     'max-empty-lines': 3,
-    'number-leading-zero': 'never'
+    'number-leading-zero': 'never',
+    'number-max-precision': 5
   }
 };
